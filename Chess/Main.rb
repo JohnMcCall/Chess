44a80f37b91ec1c5763198704d1191c7a84893b1
JohnMcCall <mcca0798@morris.umn.edu>
2014-02-03 00:49:50 -0600

    Created a chessboard image and got it to successfully display!

--- Chess/Main.rb
+++ Chess/Main.rb
@@ -2,7 +2,10 @@
 
 require "./Piece"
 require "./Board"
+require "rubygame"
 
+include Rubygame
+include Sprites::Sprite
 
 #Test Piece
 puts "Creating a new White Piece"
@@ -18,3 +21,15 @@ puts @blackPiece.getColor()
 #Test Board
 @board = Board.new
 @board.printBoard()
+
+#Test Drawing the Board
+@screen = Screen.open [640,640]
+@background = Surface.load "images/ChessBoard.jpg"
+@background.blit @screen, [0,0]
+
+@screen.flip
+## Keep the Board Drawn until a key is pressed.
+@event_queue = EventQueue.new
+@event_queue.enable_new_style_events
+until @event_queue.wait().is_a? Events::KeyPressed
+end
